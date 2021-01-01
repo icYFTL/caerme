1f555559ccd5ee342ef1dbfe7c04bf07797afa6d
icYFTL <savap0@yandex.ru>
2021-01-01 21:39:55 +0300

    Backend fixes

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,6 @@ from flask import Flask
 import requests
 import json
 from time import sleep
-from datetime import datetime
 from threading import Thread
 
 app = Flask(__name__)
@@ -11,13 +10,14 @@ ru_rating = 0
 world_rating = 0
 pts = 0
 
+
 def update():
     global ru_rating, world_rating, pts
     while True:
         data = json.loads(requests.get('https://icyftl.ru/ctftime/teams/get/team/121175/id').text)
         ru_rating = data.get('country_place')
         if data.get('rating'):
-            world_rating = data['rating'][0][str(datetime.now().year)]['rating_place']
+            world_rating = data['rating'][0].get(list(data['rating'][0].keys())[0], {}).get('rating_place', 0)  # LolKek
             pts = 0
             for x in data['rating']:
                 for key in x:
